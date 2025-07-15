e1dc50208cba2821055a0a7b5dbcfc1e8d972b12
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2025-07-15 01:11:10 -0700

    Tarea 2 del login

--- appFront/LogIn/LoginPage.xaml.cs
+++ appFront/LogIn/LoginPage.xaml.cs
@@ -6,4 +6,20 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		if (User.Text == "" || User.Text == null)
+		{
+			DisplayAlert("Error", "Falta el Usuario", "Ok");
+		}
+        else if (Password.Text == "" || Password.Text == null)
+        {
+            DisplayAlert("Error", "Falta la Contrasenia", "Ok");
+        }
+		else
+        {
+            DisplayAlert("Login Exitoso", $"El usuario {User.Text} ingreso con exito", "Ok");
+        }
+    }
 }
